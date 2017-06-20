a865a0ce4fd31a2d62ba2023b0aa3633421368ca
sangaj <gubingjing@hotmail.com>
2017-06-20 10:20:14 +0200

    Update Schiphol area.R

--- Schiphol area.R
+++ Schiphol area.R
@@ -1,20 +1,24 @@
 ### Schiphol Area Figure
 
+## loading packages
 library(ggplot2)
 library(ggmap)
 
+# choose the area and set the coordinates
 map <- qmap("2141CN",zoom = 12, maptype="toner-lite")
 l_lon <-  4.706
 r_lat <-  52.368
 r_lon <-  4.717
 l_lat <-  52.325
 #map <- map+geom_rect(aes(xmin=l_lon, xmax=r_lon, ymin=l_lat ,ymax=r_lat), fill="lightblue", alpha=0.2)
+# calculate the extended are for research
 lat_det <-  2/111
 lon_det <-  2/(cos(r_lat*pi/180)*111.321)
 bound_x <-  c(l_lon-lon_det,l_lat-lat_det)
 bound_y <-  c(r_lon+lon_det,r_lat+lat_det)
 #map <- map+geom_rect(aes(xmin=l_lon-lon_det, xmax=r_lon+lon_det, ymin=l_lat-lat_det ,ymax=r_lat+lat_det), fill="lightblue", alpha=0.2)
 
+# divided the extended area into grid
 gridx <- seq(l_lon-lon_det,r_lon+lon_det,length.out=4)
 gridy <- seq(l_lat-lat_det,r_lat+lat_det,length.out=6)
 grid <- expand.grid(x = gridx, y = gridy)
